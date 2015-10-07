b5b7ff0f6ffeca111f6c3618b9976480983645f0
Jon Brisbin <jbrisbin@basho.com>
2015-10-07 16:49:25 -0500

    Added a REST API to control downloads.

--- kafka-source/README.md
+++ kafka-source/README.md
@@ -5,16 +5,94 @@ and downloads historical data for a given range. To change what range is configu
 edit the `src/main/resources/application.properties` file and change the `mars.ingest.from`
 and `mars.ingest.to` values.
 
-To run the app, just do:
+To start the app from Gradle, use the standard Spring Boot convention for starting apps from source:
 
-    > ./gradlew bootRun
+    $ ./gradlew bootRun
 
-You should see messages flowing if consuming from Kafka using the CLI:
+Attach a consumer to Kafka using the CLI if you want to see messages flowing:
 
-    > kafka-console-consumer.sh --zookeeper 127.0.0.1:2181 --topic mars-ingest
+    $ kafka-console-consumer.sh --zookeeper 127.0.0.1:2181 --topic mars-ingest
 
-The topic name is set in the `application.properties` file as well. Spring Cloud expects
-it to be set thus:
+### Start a download
+
+To start a download of data, call the embedded REST API:
+
+    $ curl -v http://localhost:8080/start?species=CO
+    > GET /start?species=CO HTTP/1.1
+    > Host: localhost:8080
+    > User-Agent: curl/7.43.0
+    > Accept: */*
+    >
+    < HTTP/1.1 202 Accepted
+    < Server: Apache-Coyote/1.1
+    < X-Application-Context: bootstrap:8080
+    < Link: rel=info,href=/info/a46046b0-6d37-11e5-7d6b-92ca39710e50
+    < Link: rel=stop,href=/stop/a46046b0-6d37-11e5-7d6b-92ca39710e50
+    < Content-Length: 0
+
+You can give it a comma-separated string of values of species codes to start downloading,
+since you might not want to do all 11 at once (the default if you leave the parameter off).
+
+Valid species codes are:
+
+* TMP - Temperature
+* RHUM - Relative Humidity
+* SOLR - Solar Radiation
+* WDIR - Wind Direction
+* WSPD - Wind Speed
+* CO - Carbon Monoxide
+* NO2 - Nitrogen Dioxide
+* O3 - Ozone
+* PM10 - PM10 Particulates
+* PM25 - PM2.5 Particulates
+* SO2 - Sulphur Dioxide
+
+You can also give a wider date range to work with by passing `from` and `to` URL query parameters:
+
+    $ curl -v http://localhost:8080/start?species=CO&from=2015-05-01&to=2015-11-01
+
+The above would start a download of Carbon Monoxide data from May 1st, 2015 to November 1st, 2015.
+The ending data is non-inclusive, so would not contain data for November 1st, only October 31st.
+
+### Controlling the download
+
+You can find out if a download is still active (since you might not be able to see the log files
+if they're deployed in Mesos) by calling the link denoted by the "info" rel in the Link header
+returned from the `/start` request:
+
+    $ curl -v http://localhost:8080/info/a46046b0-6d37-11e5-7d6b-92ca39710e50
+    > GET /info/a46046b0-6d37-11e5-7d6b-92ca39710e50 HTTP/1.1
+    > Host: localhost:8080
+    > User-Agent: curl/7.43.0
+    > Accept: */*
+    >
+    < HTTP/1.1 200 OK
+    < Server: Apache-Coyote/1.1
+    < X-Application-Context: bootstrap:8080
+    < Content-Type: application/json;charset=UTF-8
+    < Transfer-Encoding: chunked
+    <
+    {"active":true}
+
+You can also cancel a download in progress by doing a GET of the "stop" rel:
+
+    $ curl -v http://localhost:8080/stop/a46046b0-6d37-11e5-7d6b-92ca39710e50
+    > GET /stop/82874480-6d37-11e5-b2f8-16f1e72f5fb7 HTTP/1.1
+    > Host: localhost:8080
+    > User-Agent: curl/7.43.0
+    > Accept: */*
+    >
+    < HTTP/1.1 200 OK
+    < Server: Apache-Coyote/1.1
+    < X-Application-Context: bootstrap:8080
+    < Content-Type: application/json;charset=UTF-8
+    < Transfer-Encoding: chunked
+    <
+    {"active":false}
+
+### Configuration
+
+The topic name is set in the `application.properties`. Spring Cloud expects it to be set thus:
 
     spring.cloud.stream.bindings.output=mars-ingest
 
--- kafka-source/src/main/groovy/com/basho/ingest/LondonAirQualityStreamSource.groovy
+++ kafka-source/src/main/groovy/com/basho/ingest/LondonAirQualityStreamSource.groovy
@@ -3,19 +3,24 @@ package com.basho.ingest
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.metrics.CounterService
+import org.springframework.boot.actuate.metrics.GaugeService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.messaging.Source
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.integration.support.MessageBuilder
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.bind.annotation.*
 import reactor.Environment
-import reactor.core.processor.RingBufferWorkProcessor
-import reactor.rx.Stream
+import reactor.core.processor.RingBufferProcessor
+import reactor.core.support.UUIDUtils
+import reactor.jarjar.jsr166e.extra.AtomicDouble
 import reactor.rx.Streams
-
-import java.util.concurrent.CountDownLatch
+import reactor.rx.action.Control
 
 @SpringBootApplication
 @EnableBinding(Source.class)
@@ -27,7 +32,7 @@ class LondonAirQualityStreamSource {
     }
 
     def log = LoggerFactory.getLogger(LondonAirQualityStreamSource)
-    def exitLatch = new CountDownLatch(1)
+    def totalSize = new AtomicDouble(0.0)
 
     // Inject Spring Cloud Stream Kafka Source
     @Autowired
@@ -35,83 +40,94 @@ class LondonAirQualityStreamSource {
     // Use existing Jackson ObjectMapper
     @Autowired
     ObjectMapper mapper
-    // Property to use as the start date for ingestion
-    @Value('${mars.ingest.from}')
-    String from
-    // Property to use as the end date for ingestion
-    @Value('${mars.ingest.to}')
-    String to
-    // Which species to download
-    @Value('${mars.ingest.species}')
-    String speciesCodes
+    @Autowired
+    CounterService count
+    @Autowired
+    GaugeService sizeGuage
 
-    // Parse the contents of the sites.json data only once since we're exposing this as a singleton Bean
     @Bean
-    Stream<Map> sites() {
-        Streams.from((List) mapper.readValue(new URL("http://api.erg.kcl.ac.uk/AirQuality/Information/MonitoringSites/GroupName=All/Json"), Map).Sites.Site)
-    }
-
-    // Parse the contents of the species.json data only once since we're exposing this as a singleton Bean
-    @Bean
-    Stream<String> species() {
-        Streams.from(speciesCodes.split(','))
-    }
-
-    // Create a RingBufferWorkProcessor, which is just a multi-threaded Reactive Streams Processor, to do work
-    @Bean
-    RingBufferWorkProcessor workProcessor() {
-        RingBufferWorkProcessor.create("mars-ingest", 4 * 1024)
+    List sites() {
+        mapper.readValue(new URL("http://api.erg.kcl.ac.uk/AirQuality/Information/MonitoringSites/GroupName=All/Json"), Map).Sites.Site
     }
 
     // Once the Environment has been initialized and all services are ready, invoke this method
-    def downloadAvailableSpecies() {
-        // Use the RingBuffer to divvy up work amongst several Subscribers
-        def stream = sites().process(workProcessor())
+    Control downloadAvailableSpecies(String[] species, String from, String to) {
+        // Use the RingBufferProcessor to do work in another thread
+        Streams.from(sites()).
+                process(RingBufferProcessor.create("mars-ingest", 1024, true)).
+                consume { site ->
+                    // Extract site code, which is a unique code for a sensor
+                    def siteCode = site['@SiteCode']
+                    Streams.from(species).
+                            flatMap { speciesCode ->
+                                // Pull the data for a given site + species + fromDate + toDate
+                                def url = "http://api.erg.kcl.ac.uk/AirQuality/Data/SiteSpecies/SiteCode=$siteCode/SpeciesCode=$speciesCode/StartDate=$from/EndDate=$to/Json"
 
-        // Subscribe to this Stream once per CPU slot
-        (1..Environment.PROCESSORS).each {
-            // Each Subscriber operates in its own Thread
-            stream.consume { site ->
-                // Extract site code, which is a unique code for a sensor
-                def siteCode = site['@SiteCode']
-                species().
-                        flatMap { speciesCode ->
-                            // Pull the data for a given site + species + fromDate + toDate
-                            def url = "http://api.erg.kcl.ac.uk/AirQuality/Data/SiteSpecies/SiteCode=$siteCode/SpeciesCode=$speciesCode/StartDate=$from/EndDate=$to/Json"
+                                log.info "Loading data for site: ${siteCode}, species: ${speciesCode}"
+                                log.info "  << $url"
 
-                            log.info "Loading data for site: ${siteCode}, species: ${speciesCode}"
-                            log.info "  << $url"
-
-                            Streams.from((List) mapper.readValue(new URL(url).openStream(), Map).RawAQData.Data).
-                                    map { data ->
-                                        // Add the site code and species code back into the data since it lives
-                                        // at a higher level in the object hierarchy
-                                        data['@SiteCode'] = siteCode
-                                        data['@SpeciesCode'] = speciesCode
-                                        data
-                                    }
-                        }.
-                        observeComplete { exitLatch.countDown() }.
-                        consume { data ->
-                            // For each data point, send a separate Message to the Spring Cloud Stream Source,
-                            // which means send it to Kafka in this implementation
-                            out.output().send(
-                                    MessageBuilder.withPayload(mapper.writeValueAsBytes(data)).build()
-                            )
-                        }
-            }
-        }
+                                Streams.from((List) mapper.readValue(new URL(url).openStream(), Map).RawAQData.Data).
+                                        map { data ->
+                                            // Add the site code and species code back into the data since it lives
+                                            // at a higher level in the object hierarchy
+                                            data['@SiteCode'] = siteCode
+                                            data['@SpeciesCode'] = speciesCode
+                                            data
+                                        }
+                            }.
+                            consume { data ->
+                                // For each data point, send a separate Message to the Spring Cloud Stream Source,
+                                // which means send it to Kafka in this implementation
+                                count.increment("mars.ingest.count")
+                                def bytes = mapper.writeValueAsBytes(data)
+                                sizeGuage.submit("mars.ingest.bytes", totalSize.addAndGet(bytes.length))
+                                out.output().send(MessageBuilder.withPayload(bytes).build())
+                            }
+                }
     }
 
     public static void main(String[] args) {
         // Bootstrap the app
-        def app = SpringApplication.run(LondonAirQualityStreamSource).getBean(LondonAirQualityStreamSource)
+        SpringApplication.run(LondonAirQualityStreamSource)
+    }
 
-        // Everything's ready, go do work
-        app.downloadAvailableSpecies()
+    @RestController
+    static class DataDownloadController {
 
-        // Stay active until through processing
-        app.exitLatch.await()
+        def downloads = [:]
+
+        @Autowired
+        LondonAirQualityStreamSource app
+
+        @RequestMapping(value = "/start", method = RequestMethod.GET)
+        def start(
+                @RequestParam(name = 'species', defaultValue = 'TMP,RHUM,SOLR,WDIR,WSPD,CO,NO2,O3,PM10,PM25,SO2') String speciesCodes,
+                @RequestParam(name = 'from', defaultValue = '2015-10-01') String from,
+                @RequestParam(name = 'to', defaultValue = '2015-11-01') String to
+        ) {
+            def codes = speciesCodes.split(',')
+
+            def ctl = app.downloadAvailableSpecies(codes, from, to)
+
+            def id = UUIDUtils.create().toString()
+            downloads[id] = ctl
+
+            new ResponseEntity(new LinkedMultiValueMap([
+                    Link: ["rel=info,href=/info/$id".toString(), "rel=stop,href=/stop/$id".toString()]
+            ]), HttpStatus.ACCEPTED)
+        }
+
+        @RequestMapping(value = "/info/{id}", method = RequestMethod.GET)
+        def info(@PathVariable String id) {
+            [active: downloads[id]?.publishing ?: false]
+        }
+
+        @RequestMapping(value = "/stop/{id}", method = RequestMethod.GET)
+        def stop(@PathVariable String id) {
+            downloads.remove(id)?.cancel()
+            info(id)
+        }
+
     }
 
 }
