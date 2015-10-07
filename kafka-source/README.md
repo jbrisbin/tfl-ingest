07ca0bbcc076b801adf52a39b0728816152f4385
Jon Brisbin <jbrisbin@basho.com>
2015-10-07 13:07:36 -0500

    Updates to downloading site and species data and change to message type.

--- kafka-source/README.md
+++ kafka-source/README.md
@@ -1,6 +1,9 @@
 # Ingest London Air Quality Data into Kafka
 
-This application connects to the King's College London Air Quality project website and downloads historical data for a given range. To change what range is configured, edit the `src/main/resources/application.properties` file and change the `mars.ingest.from` and `mars.ingest.to` values.
+This application connects to the King's College London Air Quality project website
+and downloads historical data for a given range. To change what range is configured,
+edit the `src/main/resources/application.properties` file and change the `mars.ingest.from`
+and `mars.ingest.to` values.
 
 To run the app, just do:
 
@@ -10,8 +13,11 @@ You should see messages flowing if consuming from Kafka using the CLI:
 
     > kafka-console-consumer.sh --zookeeper 127.0.0.1:2181 --topic mars-ingest
 
-The topic name is set in the `application.properties` file as well. Spring Cloud expects it to be set thus:
+The topic name is set in the `application.properties` file as well. Spring Cloud expects
+it to be set thus:
 
     spring.cloud.stream.bindings.output=mars-ingest
 
-Not obvious, I know. :)
+Not obvious, I know. :) That's because Spring Cloud Stream has multiple implementations
+and the "topic" value (here used with Kafka) needs to be generic across different kinds of
+stream bindings like Redis, RabbitMQ, etc...
--- kafka-source/src/main/groovy/com/basho/ingest/LondonAirQualityStreamSource.groovy
+++ kafka-source/src/main/groovy/com/basho/ingest/LondonAirQualityStreamSource.groovy
@@ -1,6 +1,6 @@
 package com.basho.ingest
 
-import org.slf4j.Logger
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -17,35 +17,41 @@ import reactor.rx.Streams
 
 @SpringBootApplication
 @EnableBinding(Source.class)
-class LondonAirQualityKafkaSource {
+class LondonAirQualityStreamSource {
 
     static {
         // Initialize Reactor's Environment (only needs to be done once somewhere in the app)
         Environment.initializeIfEmpty().assignErrorJournal()
     }
 
-    Logger log = LoggerFactory.getLogger(LondonAirQualityKafkaSource)
+    def log = LoggerFactory.getLogger(LondonAirQualityStreamSource)
+    def speciesCodes = ['TMP', 'RHUM', 'SOLR', 'WDIR', 'WSPD', 'CO', 'NO2', 'O3', 'PM10', 'PM25', 'SO2']
 
     // Inject Spring Cloud Stream Kafka Source
     @Autowired
     Source out
-    // Property to use as the start data for ingestion
-    @Value('${mars.ingest.from}')
+    // Use existing Jackson ObjectMapper
+    @Autowired
+    ObjectMapper mapper
+    // Property to use as the start date for ingestion
+    @Value('${mars.ingest.from?:2015-10-01}')
     String from
     // Property to use as the end date for ingestion
-    @Value('${mars.ingest.to}')
+    @Value('${mars.ingest.to?:2015-11-01}')
     String to
 
     // Parse the contents of the sites.json data only once since we're exposing this as a singleton Bean
     @Bean
     Stream<Map> sites() {
-        Streams.from(Map.parse("sites.json").Sites.Site)
+        def s = (List) mapper.readValue(new URL("http://api.erg.kcl.ac.uk/AirQuality/Information/MonitoringSites/GroupName=All/Json"), Map).Sites.Site
+        println "sites: $s"
+        Streams.from(s)
     }
 
     // Parse the contents of the species.json data only once since we're exposing this as a singleton Bean
     @Bean
-    Stream<Map> species() {
-        Streams.from(Map.parse("species.json").AirQualitySpecies.Species)
+    Stream<String> species() {
+        Streams.from(speciesCodes)
     }
 
     // Create a RingBufferWorkProcessor, which is just a multi-threaded Reactive Streams Processor, to do work
@@ -66,14 +72,16 @@ class LondonAirQualityKafkaSource {
                 // Extract site code, which is a unique code for a sensor
                 def siteCode = site['@SiteCode']
                 species().
-                        flatMap { species ->
+                        flatMap { speciesCode ->
                             // Extract species code, which is a unique code for a species of data
-                            def speciesCode = species['@SpeciesCode']
+                            //def speciesCode = species['@SpeciesCode']
 
                             log.info "Loading data for site: ${siteCode}, species: ${speciesCode}"
 
                             // Pull the data for a given site + species + fromDate + toDate
-                            Streams.from(Map.parse(new URL("http://api.erg.kcl.ac.uk/AirQuality/Data/SiteSpecies/SiteCode=$siteCode/SpeciesCode=$speciesCode/StartDate=$from/EndDate=$to/Json")).RawAQData.Data).
+                            def url = "http://api.erg.kcl.ac.uk/AirQuality/Data/SiteSpecies/SiteCode=$siteCode/SpeciesCode=$speciesCode/StartDate=$from/EndDate=$to/Json"
+                            log.info " << $url"
+                            Streams.from((List) mapper.readValue(new URL(url).openStream(), Map).RawAQData.Data).
                                     map { data ->
                                         // Add the site code and species code back into the data since it lives
                                         // at a higher level in the object hierarchy
@@ -86,7 +94,7 @@ class LondonAirQualityKafkaSource {
                             // For each data point, send a separate Message to the Spring Cloud Stream Source,
                             // which means send it to Kafka in this implementation
                             out.output().send(
-                                    MessageBuilder.withPayload(data.toJSON()).build()
+                                    MessageBuilder.withPayload(mapper.writeValueAsBytes(data)).build()
                             )
                         }
             }
@@ -95,7 +103,7 @@ class LondonAirQualityKafkaSource {
 
     public static void main(String[] args) {
         // Bootstrap the app
-        def app = SpringApplication.run(LondonAirQualityKafkaSource).getBean(LondonAirQualityKafkaSource)
+        def app = SpringApplication.run(LondonAirQualityStreamSource).getBean(LondonAirQualityStreamSource)
 
         // Everything's ready, go do work
         app.downloadAvailableSpecies()
